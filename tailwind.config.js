6f73842ec353e888851f2e74e9c5ec9a41f99ecc
Alexander Lysechko <alex.l@solid-sl.com>
2025-05-26 14:24:51 +0300

    add a mobile layout to the landing, update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     content: ['./*.html', './src/**/*.{html,js,scss}', './public/**/*.{html,js,scss}'],
     theme: {
+        screens: {
+            xs: '376px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
         extend: {},
     },
     plugins: [],
