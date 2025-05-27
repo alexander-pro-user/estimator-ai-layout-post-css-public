0c0b9fe3561404730a5cf058e9d296fce9c7046c
Alexander Lysechko <alex.l@solid-sl.com>
2025-05-27 11:58:34 +0300

    change project structure

--- public/main.js
+++ public/main.js
@@ -2,10 +2,25 @@
 function initHeader() {
     const menuBtn = document.getElementById('id-mobile-hamburger-button');
     const mobileMenu = document.getElementById('id-mobile-menu');
+    const loginBtn = document.getElementById('id-login-button');
+    const logoBtn = document.getElementById('id-logo-button');
+
 
     if (menuBtn && mobileMenu) {
         menuBtn.addEventListener('click', () => {
             mobileMenu.classList.toggle('hidden');
         });
+    }
+
+    if (loginBtn) {
+        loginBtn.addEventListener('click', () => {
+            window.location.href = '/estimations.html';
+        })
+    }
+
+    if (logoBtn) {
+        logoBtn.addEventListener('click', () => {
+            window.location.href = '/';
+        })
     }
 }
