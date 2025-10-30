792cd86eba066ce72046c0465f32c023b1652c2d
Alexander Lysechko <alex.l@solid-sl.com>
2025-10-30 15:38:59 +0200

    add interactivity and links

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ function initHeader() {
     const menuBtn = document.getElementById('id-mobile-hamburger-button');
     const mobileMenu = document.getElementById('id-mobile-menu');
     const loginBtn = document.getElementById('id-login-button');
+    const loginMobileBtn = document.getElementById('id-login-button-mobile');
     const logoBtn = document.getElementById('id-logo-button');
     const popupMenuButton = document.getElementById('id-popup-menu-button');
     const popupMenu = document.getElementById('id-popup-menu');
@@ -22,6 +23,12 @@ function initHeader() {
 
     if (loginBtn) {
         loginBtn.addEventListener('click', () => {
+            window.location.href = '/estimations.html';
+        })
+    }
+
+    if (loginMobileBtn) {
+        loginMobileBtn.addEventListener('click', () => {
             window.location.href = '/estimations.html';
         })
     }
