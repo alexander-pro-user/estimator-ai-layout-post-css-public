7b4f5c4a5be446fa6fd1a2a22fe6aea43f2bf0b7
Alexander Lysechko <alex.l@solid-sl.com>
2025-11-12 17:11:33 +0200

    add collapse animation, update JS

--- public/main.js
+++ public/main.js
@@ -93,8 +93,7 @@ function initSelectForCountries() {
     });
 }
 
-function initCollapseBlocks(){
-    // Get all headers of collapsible blocks
+function initCollapseBlocks() {
     const collapseHeaders = document.querySelectorAll('.custom-form-collapse-header');
 
     collapseHeaders.forEach(header => {
@@ -105,28 +104,31 @@ function initCollapseBlocks(){
             // Find the block body
             const collapseBody = collapseBlock.querySelector('.custom-form-collapse-body');
 
-            // Checking current display status
-            if (collapseBody.style.display === 'none') {
-                // Разворачиваем блок
-                collapseBody.style.display = 'block';
+            // toggle active class
+            collapseBlock.classList.toggle('custom-form-collapse--active');
+
+            if (collapseBlock.classList.contains('custom-form-collapse--active')) {
+                // Expand
+                collapseBody.style.maxHeight = collapseBody.scrollHeight + 'px';
                 collapseBlock.querySelector('.custom-form-collapse-header-image img:first-child').style.display = 'none';
                 collapseBlock.querySelector('.custom-form-collapse-header-image img:last-child').style.display = 'inline';
             } else {
                 // Collapse block
-                collapseBody.style.display = 'none';
+                collapseBody.style.maxHeight = '0';
                 collapseBlock.querySelector('.custom-form-collapse-header-image img:first-child').style.display = 'inline';
                 collapseBlock.querySelector('.custom-form-collapse-header-image img:last-child').style.display = 'none';
             }
         });
     });
 
-    // Initially hide block bodies
+    // Initial setup
     const collapseBodies = document.querySelectorAll('.custom-form-collapse-body');
     collapseBodies.forEach(body => {
-        body.style.display = 'none';
+        body.style.maxHeight = '0';
+        body.style.overflow = 'hidden';
+        body.style.transition = 'max-height 0.2s ease';
     });
 
-    // Initially, we show the advantages and hide the disadvantages.
     const headerImages = document.querySelectorAll('.custom-form-collapse-header-image img');
     headerImages.forEach((img, index) => {
         if (index % 2 === 0) {
