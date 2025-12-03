0faa1202fd0305b486e0ff696285aff87afce8bb
Alexander Lysechko <alex.l@solid-sl.com>
2025-12-03 12:34:44 +0200

    add a button column to the support-requests page, update the popup on the support-requests page

--- public/main.js
+++ public/main.js
@@ -172,6 +172,47 @@ async function initShowMorePopup() {
     });
 }
 
+async function initShowMorePopupSupportRequests() {
+    const popupHtml = await fetch('/components/popup-menu-support-requests.html')
+        .then(r => r.text());
+
+    const popupMenus = document.querySelectorAll('.popup-menu');
+
+    popupMenus.forEach(el => {
+        el.innerHTML = popupHtml;
+
+        const cancelButton = el.querySelector('.cancel-popup-button');
+        cancelButton.addEventListener('click', () => {
+            el.classList.add('hidden');
+        });
+    });
+
+    const buttons = document.querySelectorAll('.popup-menu-button');
+    buttons.forEach(btn => {
+        btn.addEventListener('click', e => {
+            const popup = btn.nextElementSibling;
+
+            popupMenus.forEach(p => {
+                if (p !== popup) {
+                    p.classList.add('hidden');
+                }
+            });
+
+            popup.classList.toggle('hidden');
+        });
+    });
+
+    document.addEventListener('click', e => {
+        if (e.target.closest('.popup-menu-button')) return;
+
+        popupMenus.forEach(popup => {
+            if (!popup.contains(e.target)) {
+                popup.classList.add('hidden');
+            }
+        });
+    });
+}
+
 async function initShowMoreEstimationPagePopup() {
     const popupHtml = await fetch('/components/popup-menu-estimation-page.html')
         .then(r => r.text());
